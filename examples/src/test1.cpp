edd07eefce5ca38437c2b691c67e1db43009c89c
sv <in@vasyut.in>
2022-12-17 14:08:38 +0300

    Manual fixes

---
+++ examples/src/test1.cpp
@@ -0,0 +1,45 @@
+// -*- Mode: c++; c-basic-offset: 4; tab-width: 4; -*-
+
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include "wtclap/CmdLine.h"
+
+using namespace TCLAP;
+using namespace std;
+
+int wmain(int argc, wchar_t **argv) {
+    // Wrap everything in a try block.  Do this every time,
+    // because exceptions will be thrown for problems.
+    try {
+        // Define the command line object.
+        CmdLine cmd(L"Command description message", L' ', L"0.9");
+
+        // Define a value argument and add it to the command line.
+        ValueArg<wstring> nameArg(L"n", L"name", L"Name to print", true, L"homer",
+                                 L"string");
+        cmd.add(nameArg);
+
+        // Define a switch and add it to the command line.
+        SwitchArg reverseSwitch(L"r", L"reverse", L"Print name backwards", false);
+        cmd.add(reverseSwitch);
+
+        // Parse the args.
+        cmd.parse(argc, argv);
+
+        // Get the value parsed by each arg.
+        wstring name = nameArg.getValue();
+        bool reverseName = reverseSwitch.getValue();
+
+        // Do what you intend too...
+        if (reverseName) {
+            reverse(name.begin(), name.end());
+            wcout << L"My name (spelled backwards) is: " << name << endl;
+        } else
+            wcout << L"My name is: " << name << endl;
+
+    } catch (ArgException &e)  // catch any exceptions
+    {
+        wcerr << "error: " << e.error() << " for arg " << e.argId() << endl;
+    }
+}
